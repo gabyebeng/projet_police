faeb2fe4febc4e07fe19147c7f96e96784dd7bd9
GabyEbeng <gabyebeng@gamil.com>
2024-08-24 22:59:11 +0100

    modification du view policier

--- resources/views/Pages/policier.blade.php
+++ resources/views/Pages/policier.blade.php
@@ -1,5 +1,48 @@
 @extends('layouts.template')
 @section('content')
+    <div class="container-xl mb-2">
+        {{-- <h1 class="app-page-title">Liste des policiers de la base de données</h1> --}}
+        <hr class="mb-2">
+        <div class="card">
+            <div class="card-header">
+                <h3>Operations Disponibles</h3>
+            </div>
+            <div class="card-body">
+                <div class="row g-4 settings-section">
+                    <div class="col-12 col-md-6">
+                        <h3 class="section-title">Importez la liste</h3>
+                        <div class="section-intro">
+                            <form action="" method="post" enctype="multipart/form-data">
+                                <div class="input-group"><input class="form-control" type="file" name="excel_file"
+                                        id="">
+                                    <input class="btn btn-primary" type="submit" value="Importer">
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <h3 class="section-title">Recherchez un policier</h3>
+                        <div class="section-intro">
+                            <form action="{{ route('policier.search') }}" method="post">
+                                @csrf
+                                @method('post')
+                                <div class="input-group">
+                                    <input type="text" placeholder="Recherche ..." name="recherche"
+                                        class="form-control" />
+                                    <button type="submit" class="btn btn-success">
+                                        <i class="fa fa-search search-icon"> Recherche</i>
+                                    </button>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--//row-->
+    </div><!--//container-fluid-->
+
     <div class="col-md-12">
         <div class="card card-round">
             <div class="card-header">
@@ -8,23 +51,14 @@
                         @csrf
                         @method('POST')
                         <nav class="navbar navbar-header-left navbar-expand-lg navbar-form nav-search p-0 d-none d-lg-flex">
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <button type="submit" class="btn btn-search pe-1">
-                                        <i class="fa fa-search search-icon"></i>
-                                    </button>
-                                </div>
-                                <input type="text" placeholder="Recherche ..." name="recherche" class="form-control" />
-                                <button type="submit" class="btn btn-success">
-                                    <i class="fa fa-search search-icon"> Recherche</i>
-                                </button>
-                            </div>
+
                         </nav>
                     </form>
 
                     <div class="card-title">Policiers dans la base des données</div>
-
                 </div>
+
+
             </div>
             <div class="card-body ">
                 <div class="table-responsive">
